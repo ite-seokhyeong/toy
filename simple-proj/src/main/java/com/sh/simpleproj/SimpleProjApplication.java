1d0d9e04bba0c397f81c6cc6cadc96692f9574dc
itesh <ite.seokhyeong@gmai.com>
2020-06-07 23:41:05 +0900

    logback 설정 추가

--- simple-proj/src/main/java/com/sh/simpleproj/SimpleProjApplication.java
+++ simple-proj/src/main/java/com/sh/simpleproj/SimpleProjApplication.java
@@ -1,16 +1,24 @@
 package com.sh.simpleproj;
 
+
 import com.sh.simpleproj.config.WebServerConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class SimpleProjApplication {
 
+	private static final Logger logger  = LoggerFactory.getLogger(SimpleProjApplication.class);
+
 	public static void main(String[] args) {
+		logger.info("!!!!!!!!Sping boot Logback {}", SimpleProjApplication.class.getSimpleName());
 		new SpringApplicationBuilder(SimpleProjApplication.class)
 				.initializers(new WebServerConfig())
 				.run();
+
+
 		//SpringApplication.run(SimpleProjApplication.class, args);
 	}
 
--- simple-proj/src/main/java/com/sh/simpleproj/controller/SimpleController.java
+++ simple-proj/src/main/java/com/sh/simpleproj/controller/SimpleController.java
@@ -1,15 +1,35 @@
 package com.sh.simpleproj.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class SimpleController {
 
+    static final Logger logger = LoggerFactory.getLogger(SimpleController.class);
+
     @RequestMapping("/home")
     public String home() {
-        //기본페이지
+        logger.info("home()!");
         return "index.html";
+    }
+
+    /** TODO
+     * 로그 내용에 따라 적절한 로그 레벨을 적용합니다.
+     * 오류 발생 시, StackTrace 전체를 로그 파일에 남깁니다.
+     */
+
+    @GetMapping("/error")
+    public void logbackTest() {
+        try {
+            throw  new Exception("에러발생시킴");
+        } catch (Exception e) {
+            //e.printStackTrace();
+            logger.error("message", e);
+        }
     }
 
 }
